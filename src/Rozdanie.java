07d31eca3b8c98c52240488c86d46f86456c17e8
Piotr Fej <p.fejcher@gmail.com>
2019-06-30 12:44:26 +0200

    1 commit

---
+++ src/Rozdanie.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class Rozdanie implements I_rozdanie
+{
+    Karta karta;
+    Stack<Karta> talia = new Stack<>();
+    Stack<Karta> reka = new Stack<>();
+    Stol stol;
+    Gracz gracz;
+    int iloscGraczy;
+
+
+    @Override
+    public void rozdaj()
+    {
+    }
+}
